cf748d3f28f0cee09e761aafc607896b0419b807
andreicalinciuc <scoala.andrei@yahoo.com>
2021-05-12 22:43:16 +0300

    add rest struct Restaurant

--- models/models.go
+++ models/models.go
@@ -14,6 +14,18 @@ type User struct {
 	Email     string             `bson:"email" json:"email"`
 }
 
+type File struct {
+	Name         string `bson:"name" json:"name"`
+	OriginalName string `bson:"originalName" json:"originalName"`
+	Path         string `bson:"path" json:"path"`
+}
+
+type SocialProfile struct {
+	Facebook  string `bson:"facebook" json:"facebook"`
+	Instagram string `bson:"instagram" json:"instagram"`
+	Twitter   string `bson:"twitter" json:"twitter"`
+}
+
 type Restaurant struct {
 	Id          primitive.ObjectID `bson:"_id" json:"id"`
 	Name        string             `bson:"name" json:"name"`
@@ -22,7 +34,12 @@ type Restaurant struct {
 	Phone       string             `bson:"phone" json:"phone"`
 	Program     []Program          `bson:"program" json:"program"`
 	PostCode    int                `bson:"postCode"  json:"postCode"`
-	DeleteAt    time.Time          `bson:"deleteAt" json:"delete_at"`
+	Logo        File               `bson:"logo" json:"logo"`
+	Country     string             `bson:"country" json:"country"`
+	Email       string             `bson:"email" json:"email"`
+	Website     string             `json:"website" bson:"website"`
+	SocialProfile
+	DeleteAt time.Time `bson:"deleteAt" json:"delete_at"`
 }
 
 type Program struct {
